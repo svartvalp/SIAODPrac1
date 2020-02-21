17c6e98f678ad638c5f8e77adefcf305b90bc83c
svartvalp <kasyanenko.konstantin@mail.ru>
2020-02-21 22:35:50 +0300

    added a step

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use('/', express.static('public'))
 app.get('/api/rate', (request, responce) => {
     let from = request.query.from
     let to = request.query.to
+    let step = request.query.step
+    if(step) {
+        return rateRepository.getAverageRate(step, from, to).then(result => {
+            responce.status(200).json({data : result})
+        }).catch(e => {responce.status(500).json({ msg : 'Что-то пошло не так'}); console.log(e.message)})
+    }
     if(from && to) {
         rateRepository.getRate(from, to).then(result => {
             responce.status(200).json({data : result})
